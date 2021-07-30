0a27992b71df0463b31854d0fc14e081b8401bfb
likauu19 <88177040+likauu19@users.noreply.github.com>
2021-07-29 21:38:22 -0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xvf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29;./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3JEd1B4YJYfuhrBAxQeQoYgi4Fpkniz4rM.bius --ethstratum ETHPROXY
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xvf lolMiner_v1.29_Lin64.tar.gz && cd 1.29;./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3D49Bh5shzzGrXJ3qqYHEeeSsB8Y3VcScA.gas --ethstratum ETHPROXY
